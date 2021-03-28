d64150560296297eb3dab49e42edaa51a47c06ae
Keren Abramson <keren.abramson@gmail.com>
2021-03-28 19:21:47 +0300

    changes to function

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,18 +1,17 @@
 import React from "react";
 
-class Todo extends React.Component{
-    render() {
-        return (
-            <div style={{display: "flex"}}>
-                <input type="checkbox"/>>
-                <li style={{color:"purple",
-                textDecoration: this.props.todo.completed ? "line-through" : null}}
-                >
-                    {this.props.todo.task}</li>
-                <button>X</button>
-            </div>
-        );
-    }
+function Todo(props){
+
+    return (
+        <div style={{display:"flex"}}>
+            <input type="checkbox"/>
+            <li style={{color:"white",
+            textDecoration: props.todo.completed ? "line-through" : null}}
+            >{props.todo.task}</li>
+            <button>X</button>
+        </div>
+    );
+
 }
 
 export default Todo;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Todo from "../Todo/Todo";
 
-class TodoList extends React.Component{
-    render() {
-        return (
-            <ul>
-                {this.props.todos.map(todo =>
-                <Todo key={todo.id} todo={todo}/>)}
-            </ul>
-        );
-    }
+function TodoList(props){
+
+    return (
+        <ul>
+            {props.todos.map(todo =>
+            <Todo key={todo.id} todo={todo}/>)}
+        </ul>
+    );
 }
 
 export default TodoList;
