60408707e8496f21e53bafab9f3578fc991d640c
Keren Abramson <keren.abramson@gmail.com>
2021-04-01 21:09:12 +0300

    changed list color

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,20 @@
 import {List} from "@material-ui/core";
 import React from "react";
 import Todo from "../Todo/Todo";
+import {makeStyles} from '@material-ui/core/styles';
+import '../../App.css';
+
+const useStyles = makeStyles((theme) => ({
+    List: {
+        backgroundColor: theme.palette.list.main
+    }
+  }));
 
 function TodoList(props){
+    const classes= useStyles();
 
     return (
-        <List>
+        <List className={classes.List}>
             {props.todos.map(todo =>
             <Todo key={todo.id} todo={todo} toggleComplete={props.toggleComplete} removeTodo={props.removeTodo} />)}
         </List>
